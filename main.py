661877c80df3448a2773bdbb7ca10964c8868602
tuval7 <tobias.flock@gmx.de>
2025-09-02 12:11:32 +0200

    Programmierung der Main-Methode/Eingabe des Starts

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ Created on Tue Jul 15 13:59:24 2025
 @author: tuval7
 """
 
-import numpy as np
-import constants
 from particle import Particle
-from box import Box
 from simulation import Simulation
 
-def main():
+if __name__ == "__main__":
+
+    particle_list = [
+        Particle(1.0, 45.0, 10.0, 0.0),
+        Particle(99.0, 55.0, -10.0, 0.0),
+        Particle(10.0, 50.0, 15.0, -15.0),
+        Particle(20.0, 30.0, -15.0, -15.0),
+        Particle(80.0, 70.0, 15.0, 15.0),
+        Particle(80.0, 60.0, 15.0, 15.0),
+        Particle(80.0, 50.0, 15.0, 15.0)]
     
+    sim = Simulation(particle_list)
+    sim.run()
